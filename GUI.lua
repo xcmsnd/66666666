40f0dde3e6cb0c8ded6fddd9df2ba30b5bb75815
xcmsnd <2627272316@qq.com>
2024-12-29 14:57:36 +0800

    Add files via upload

---
+++ GUI.lua
@@ -0,0 +1,10 @@
+local DrRayLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/AZYsGithub/DrRay-UI-Library/main/DrRay.lua"))()
+
+local window = DrRayLibrary:Load("时间", "更新中")
+
+local tab = DrRayLibrary.newTab("精通1", "ImageIdHere")
+
+tab.newButton("隐身", "点击使用", function()
+    
+loadstring(game:HttpGet('https://pastebin.com/raw/3Rnd9rHf'))()
+end)
